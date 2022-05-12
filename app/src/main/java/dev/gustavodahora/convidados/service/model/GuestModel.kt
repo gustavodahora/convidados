7c23a4cb8e2cab93843f561cc406fc680971cfe3
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2022-05-12 06:08:17 -0300

    Adding a function to repository.

---
+++ app/src/main/java/dev/gustavodahora/convidados/service/model/GuestModel.kt
@@ -0,0 +1,6 @@
+package dev.gustavodahora.convidados.service.model
+
+data class GuestModel(
+    var name: String,
+    var presence: Boolean
+)
--- app/src/main/java/dev/gustavodahora/convidados/view/GuestFormActivity.kt
+++ app/src/main/java/dev/gustavodahora/convidados/view/GuestFormActivity.kt
@@ -3,6 +3,8 @@ package dev.gustavodahora.convidados.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import dev.gustavodahora.convidados.viewModel.GuestFormViewModel
 import dev.gustavodahora.convidados.R
@@ -21,13 +23,26 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
         viewModel = ViewModelProvider(this)[GuestFormViewModel::class.java]
 
         setListeners()
+        observe()
     }
 
     override fun onClick(v: View?) {
         val id = v?.id
         if (id == R.id.button_save) {
-
+            val name = binding.editName.toString()
+            val presence = binding.radioPresence.isChecked
+            viewModel.save(name, presence)
         }
+    }
+
+    private fun observe() {
+        viewModel.saveGuest.observe(this, Observer {
+            if (it) {
+                Toast.makeText(applicationContext, "Sucesso", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Falha", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     private fun setListeners() {
